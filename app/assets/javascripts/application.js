dfbdda0c78c5fd974247ba94f24de1fa28eceff1
James Kerr <jkerr838@gmail.com>
2015-08-03 12:21:03 -0700

    Slides now loop and display only for their intended duration.
    Fixes #1 and Fixes #2

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,4 +18,5 @@
 //= require moment
 
 /* APPLICATION */
+//= require device
 //= require devices
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :devices
   end
+
+  root 'devices#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
