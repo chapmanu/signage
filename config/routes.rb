dc5347177cf2eaae25742563c4067118a92b2f7d
James Kerr <jkerr838@gmail.com>
2015-08-07 16:53:41 -0700

    Root to admin panel

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
 
   resources :slides
 
-  root 'devices#index'
+  root 'admin#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
