719d0dbc294134f103a27acd3dfeb9bd4b378327
James Kerr <jkerr838@gmail.com>
2015-08-07 16:01:38 -0700

    Typo

--- app/models/person_slide.rb
+++ app/models/person_slide.rb
@@ -2,5 +2,5 @@ class PersonSlide < ActiveRecord::Base
   belongs_to :person
   belongs_to :slide
 
-  validate_uniquess_of :slide_id, :scope => :person_id, :message => "duplicate people not allowed"
+  validates_uniquess_of :slide_id, :scope => :person_id, :message => "duplicate people not allowed"
 end
