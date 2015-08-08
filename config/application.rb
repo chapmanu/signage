24a293fe7850af528a253e2adbcd7ecbe6634fc5
James Andrew Kerr <jkerr838@gmail.com>
2015-08-08 12:46:59 -0700

    Renamed app to 'signage'

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Ds
+module Signage
   class Application < Rails::Application
     config.asset_url = 'http://www2.chapman.edu'
     # Settings in config/environments/* take precedence over those specified here.
