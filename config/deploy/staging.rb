98e3ed4d2043801af6211f46c3aad3136c5272d8
James Kerr <jkerr838@gmail.com>
2015-08-10 08:46:12 -0700

    Changed the servers to use proper subdomains

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -6,7 +6,7 @@
 # server 'example.com', user: 'deploy', roles: %w{app db web}, my_property: :my_value
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
-server 'DSV-MKT-DEV01.chapman.edu', user: 'charles', roles: %w{app db web}
+server 'dev-signage.chapman.edu', user: 'charles', roles: %w{app db web}
 
 
 
