b11893bc62524b91f0c2d3cb82e63c6f7b7c4e25
James Kerr <jkerr838@gmail.com>
2015-08-12 13:41:05 -0700

    Added some live previewing to the slide editor.

--- app/assets/javascripts/slides.js
+++ app/assets/javascripts/slides.js
@@ -1,4 +1,4 @@
-Utils.fireWhenReady(['slides#preview'], function(e) {
+Utils.fireWhenReady(['slides#preview', 'slides#live_preview'], function(e) {
   $('.ui-feed').css({width: '1920px', height: '1080px'});
   var scaleSlidePreview = $.throttle(50, function() {
     var
@@ -6,7 +6,6 @@ Utils.fireWhenReady(['slides#preview'], function(e) {
     scale_y = Math.min(window.innerHeight / 1080, 1),
     scale   = Math.min(scale_x, scale_y);
     if (window.innerWidth <= 1760) {
-      console.log('need to scale');
       $('.ui-feed').css('transform', 'scale('+scale+')');
     } else {
       $('.ui-feed').css('transform', '');
