4d992bf2d85d16678dbd31efb5c0da38064147fa
James Kerr <jkerr838@gmail.com>
2015-08-12 14:03:26 -0700

    Added some friendlier ids

--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -70,7 +70,7 @@ class DevicesController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_device
-      @device = Device.find_by_name(params[:id])
+      @device = Device.friendly.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
