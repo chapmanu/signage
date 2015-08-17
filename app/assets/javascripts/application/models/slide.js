cfaba697c72eeae3ea16ee2e4ab29de2186294af
James Kerr <jkerr838@gmail.com>
2015-08-17 11:11:23 -0700

    Added video support

--- app/assets/javascripts/application/models/slide.js
+++ app/assets/javascripts/application/models/slide.js
@@ -3,27 +3,26 @@ var Slide = function($element) {
   this.behaviors = [];
 
   if (/directory/.test(this.meta('template'))) {
-    this.behaviors.push(
-      $.extend({}, autoscrollable, {
-        container:        '.ui-slide-collection',
-        items:            '.ui-member',
-        visible_rows:    4,
-        items_per_row:   4,
-        rows_per_scroll: 2,
-        scroll_interval: 8000
-      })
-    );
+    this.addBehavior('autoscrollable', {
+      container:        '.ui-slide-collection',
+      items:            '.ui-member',
+      visible_rows:    4,
+      items_per_row:   4,
+      rows_per_scroll: 2,
+      scroll_interval: 8000
+    });
+
   } else if (/schedule/.test(this.meta('template'))) {
-    this.behaviors.push(
-      $.extend({}, autoscrollable, {
-        container:        '.ui-slide-collection',
-        items:            '.ui-event',
-        visible_rows:    4,
-        items_per_row:   1,
-        rows_per_scroll: 1,
-        scroll_interval: 1000
-      })
-    );
+    this.addBehavior('autoscrollable', {
+      container:        '.ui-slide-collection',
+      items:            '.ui-event',
+      visible_rows:    4,
+      items_per_row:   1,
+      rows_per_scroll: 1,
+      scroll_interval: 1000
+    });
+  } else if (/standard/.test(this.meta('template'))) {
+    this.addBehavior('video', {});
   }
   var self = this;
   $.each(this.behaviors, function(i, behavior) { behavior.initialize(self); });
@@ -44,3 +43,7 @@ Slide.prototype.stop = function() {
 Slide.prototype.meta = function(key) {
   return this.$element.find('.js-slide-meta').data(key);
 };
+
+Slide.prototype.addBehavior = function(name, options) {
+  this.behaviors.push($.extend({}, SlideBehaviors[name], options));
+}
