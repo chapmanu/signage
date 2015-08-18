95a744f910eeb23c2f8d1e9d80bbb40619df7612
James Kerr <jkerr838@gmail.com>
2015-08-18 10:07:28 -0700

    Added active device icon to slide listings.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150813215403) do
+ActiveRecord::Schema.define(version: 20150818162225) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 20150813215403) do
     t.datetime "created_at",          null: false
     t.datetime "updated_at",          null: false
     t.string   "slug"
+    t.datetime "last_ping"
   end
 
   add_index "devices", ["slug"], name: "index_devices_on_slug", unique: true, using: :btree
--- test/models/device_test.rb
+++ test/models/device_test.rb
@@ -23,36 +23,60 @@ class DeviceTest < ActiveSupport::TestCase
 
   test "#active_slides with play_on and stop_on" do
     @slide.update(show: true, play_on: Time.zone.parse('1/1/2015 1:00pm'), stop_on: Time.zone.parse('1/5/2015 1:00pm'))
-    travel_to Time.zone.parse('1/1/2015 12:59pm')
-    @device.slides << @slide
-    assert_equal(0, @device.active_slides.count)
-    travel_to Time.zone.parse('1/1/2015 1:01pm')
-    assert_equal(1, @device.active_slides.count)
-    travel_to Time.zone.parse('1/5/2015 1:01pm')
-    assert_equal(0, @device.active_slides.count)
+    travel_to Time.zone.parse('1/1/2015 12:59pm') do
+      @device.slides << @slide
+      assert_equal(0, @device.active_slides.count)
+    end
+    travel_to Time.zone.parse('1/1/2015 1:01pm') do
+      assert_equal(1, @device.active_slides.count)
+    end
+    travel_to Time.zone.parse('1/5/2015 1:01pm') do
+      assert_equal(0, @device.active_slides.count)
+    end
   end
 
   test "#active_slides with only play_on" do
     @slide.update(show: true, play_on: Time.zone.parse('1/1/2015 1:00pm'), stop_on: nil)
-    travel_to Time.zone.parse('1/1/2015 12:59pm')
-    @device.slides << @slide
-    assert_equal(0, @device.active_slides.count)
-    travel_to Time.zone.parse('1/1/2020 1:01pm')
-    assert_equal(1, @device.active_slides.count)
+    travel_to Time.zone.parse('1/1/2015 12:59pm') do
+      @device.slides << @slide
+      assert_equal(0, @device.active_slides.count)
+    end
+    travel_to Time.zone.parse('1/1/2020 1:01pm') do
+      assert_equal(1, @device.active_slides.count)
+    end
   end
 
   test "#active_slides with only stop_on" do
     @slide.update(show: true, stop_on: Time.zone.parse('1/5/2015 1:00pm'))
-    travel_to Time.zone.parse('1/1/2015 12:59pm')
-    @device.slides << @slide
-    assert_equal(1, @device.active_slides.count)
-    travel_to Time.zone.parse('1/5/2015 1:01pm')
-    assert_equal(0, @device.active_slides.count)
+    travel_to Time.zone.parse('1/1/2015 12:59pm') do
+      @device.slides << @slide
+      assert_equal(1, @device.active_slides.count)
+    end
+    travel_to Time.zone.parse('1/5/2015 1:01pm') do
+      assert_equal(0, @device.active_slides.count)
+    end
   end
 
   test '#active_slides without any play_on/stop_ons' do
     @slide.update(show: true, play_on: nil, stop_on: nil)
     @device.slides << @slide
     assert_equal(1, @device.active_slides.count)
+  end
+
+  test '#active? returns true' do
+    @device.touch_last_ping
+    assert @device.active?
+  end
+
+  test '#active? returns false' do
+    travel_to (Time.zone.now - 8.seconds) do
+      @device.touch_last_ping
+    end
+    assert_not @device.active?
+  end
+
+  test '#active? with nil returns false' do
+    @device.last_ping = nil
+    assert_not @device.active?
   end
 end
