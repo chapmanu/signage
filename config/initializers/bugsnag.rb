2c257b062b978df3e5dbc0492e6402146b15fed6
James Kerr <jkerr838@gmail.com>
2015-09-03 14:30:15 -0700

    Updated the slide parsing so that we can get the new data from cascade.

--- config/initializers/bugsnag.rb
+++ config/initializers/bugsnag.rb
@@ -1,4 +1,4 @@
-unless Rails.env.development?
+unless Rails.env.development? || Rails.env.test?
   Bugsnag.configure do |config|
     config.api_key = "a6d7d09c72fcc4d57ed110a2c1c5a834"
   end
--- test/jobs/fetch_device_data_job_test.rb
+++ test/jobs/fetch_device_data_job_test.rb
@@ -3,13 +3,13 @@ require 'test_helper'
 class FetchDeviceDataJobTest < ActiveJob::TestCase
 
   test "it saves slides" do
-    assert_difference 'Slide.count', 6 do
+    assert_difference 'Slide.count', 8 do
       perform_job_with_people
     end
   end
 
   test "it saves scheduled items" do
-    assert_difference 'ScheduledItem.count', 4 do
+    assert_difference 'ScheduledItem.count', 2 do
       perform_job_with_scheduled_items
     end
   end
@@ -18,7 +18,7 @@ class FetchDeviceDataJobTest < ActiveJob::TestCase
     perform_job_with_people
     s = Slide.last
     assert_equal('standard', s.template)
-    assert_equal('dark', s.theme)
+    assert_equal('light', s.theme)
     assert_equal('right', s.layout)
   end
 
@@ -26,16 +26,25 @@ class FetchDeviceDataJobTest < ActiveJob::TestCase
     skip "Don't have the data from Mandy Yet"
   end
 
-  test "it saves the expired at time" do
-    skip "Don't have the data from Mandy Yet"
+  test "it saves play_on and stop_on" do
+    perform_job_with_people
+    s = Slide.where(name: 'digital-signage/slides/smc/follow-us').first
+    assert_equal(DateTime.new(2015, 8, 26), s.play_on)
+    assert_equal(DateTime.new(2015, 9, 11, 23, 59), s.stop_on)
   end
 
-  test "it saves the start playing at time" do
-    skip "Don't have the data from Mandy Yet"
+  test "its saves scheduled items play_on and stop_on" do
+    perform_job_with_scheduled_items
+    s = ScheduledItem.where(name: 'September Information Session').first
+    assert_equal(DateTime.new(2015, 8, 20), s.play_on)
+    assert_equal(DateTime.new(2015, 8, 20, 23, 59, 59), s.stop_on)
   end
 
-  test "it saves the start and stop time of scheduled items" do
-    skip "Don't have the data from Mandy Yet"
+  test "it saves play_on as nil if it is blank" do
+    perform_job_with_people
+    s = Slide.where(name: 'digital-signage/slides/copa/faculty-directory-bertea').first
+    assert_equal(nil, s.play_on)
+    assert_equal(nil, s.stop_on)
   end
 
   private
