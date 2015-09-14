dc478b96de41d3e8b66c66b9985d53fa5db91097
James Kerr <jkerr838@gmail.com>
2015-09-14 14:37:06 -0700

    Cleaned up the login page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,5 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!
   layout 'admin'
 
   # GET /users
