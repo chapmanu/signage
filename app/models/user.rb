964239fb2e58ac7bb9c2818c6a7694bd4507e462
James Kerr <jkerr838@gmail.com>
2015-09-15 10:41:13 -0700

    Added owners to devices and slides.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,19 @@
 class User < ActiveRecord::Base
+  has_many :device_users, dependent: :destroy
+  has_many :devices, through: :device_users
+  has_many :slides, -> { uniq }, through: :devices
+
   devise :database_authenticatable, :rememberable, :trackable
 
-  attr_accessor :encrypted_password
+  enum roll: { super_admin: 0 }
+
+  attr_accessor :encrypted_password # Just to make :database_authenticatable work
+
+  def devices
+    super_admin? ? Device.all : super
+  end
+
+  def slides
+    super_admin? ? Slide.all : super
+  end
 end
