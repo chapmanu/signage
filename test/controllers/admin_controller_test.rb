06b8cd832892a529496d766a97ff792e4f2785f3
James Kerr <jkerr838@gmail.com>
2015-09-14 17:01:44 -0700

    Added test helpers for devise and user auth

--- test/controllers/admin_controller_test.rb
+++ test/controllers/admin_controller_test.rb
@@ -1,6 +1,12 @@
 require 'test_helper'
 
 class AdminControllerTest < ActionController::TestCase
+  include Devise::TestHelpers
+
+  setup do
+    sign_in users(:one)
+  end
+
   test "should get index" do
     get :index
     assert_response :success
