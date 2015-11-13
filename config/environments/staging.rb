1985aa65f59cf4adaecbb849c6a25f95e535d8d9
James Kerr <jkerr838@gmail.com>
2015-11-13 11:05:10 -0800

    Add default hosts.

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -77,3 +77,4 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
+Rails.application.routes.default_url_options[:host] = 'dev-signage.chapman.edu'
