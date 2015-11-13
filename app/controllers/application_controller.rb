7bcaf7d696fd566e56c10e406a07ef173dce781b
James Kerr <jkerr838@gmail.com>
2015-11-13 13:35:32 -0800

    Fixed redirect routes. Closes #64

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
   def after_sign_in_path_for(resource)
-    admin_index_path
+    signs_path
   end
 
   rescue_from CanCan::AccessDenied do |exception|
