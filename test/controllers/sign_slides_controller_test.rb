be8c8d679a0be817d34752e5ea4978db9d1f918f
James Kerr <jkerr838@gmail.com>
2015-11-23 09:44:15 -0800

    Workflow works closes #76

--- test/controllers/sign_slides_controller_test.rb
+++ test/controllers/sign_slides_controller_test.rb
@@ -13,8 +13,20 @@ class SignSlidesControllerTest < ActionController::TestCase
     assert @sign_slide.reload.approved?, "It did not approve the slide"
   end
 
+  test 'approving a sign sends and email' do
+    assert_difference('ActionMailer::Base.deliveries.length', 1) do
+      post :approve, id: @sign_slide, format: :js
+    end
+  end
+
   test "rejecting a sign slide deletes it" do
     delete :reject, id: @sign_slide, format: :js
     assert_not SignSlide.exists?(@sign_slide.id), "Sign slide was not destroyed"
+  end
+
+  test 'rejecting a sign sends an email' do
+    assert_difference('ActionMailer::Base.deliveries.length', 1) do
+      delete :reject, id: @sign_slide, format: :js
+    end
   end
 end
