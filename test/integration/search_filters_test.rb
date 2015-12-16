654fa7e5685016efb788b044f03dd81a168d5250
James Kerr <jkerr838@gmail.com>
2015-12-15 16:39:18 -0800

    Added default profile picture for faculty.  Closes ##100

--- test/integration/search_filters_test.rb
+++ test/integration/search_filters_test.rb
@@ -4,6 +4,9 @@ class SearchFiltersTest < ActionDispatch::IntegrationTest
 
   test "saves last filter selection in cookies" do
     sign_in users(:james)
-
+    visit slides_path
+    within '.search-filters-container' do
+      ap all('.active').map(&:text)
+    end
   end
 end
