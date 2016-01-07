74a398f29d1accf9eaa33e1624632be3b32b3b24
James Kerr <jkerr838@gmail.com>
2016-01-07 09:42:00 -0800

    Only show development and test in the repo

--- config/deploy.rb
+++ config/deploy.rb
@@ -26,7 +26,7 @@ set :deploy_to, '/var/www/signage'
 # set :pty, true
 
 # Default value for :linked_files is []
-# set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
+set :linked_files, fetch(:linked_files, []).push('config/database.yml')
 
 # Default value for linked_dirs is []
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'public/uploads')
