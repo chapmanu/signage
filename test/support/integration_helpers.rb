2d85eeb04bccd9ae4a552cc7032c768761bc4679
James Kerr <jkerr838@gmail.com>
2016-02-02 11:03:37 -0800

    Rewrote tests with capybara and introduced selenium

--- test/support/integration_helpers.rb
+++ test/support/integration_helpers.rb
@@ -1,7 +1,7 @@
 module IntegrationHelpers
   def sign_in(user)
     Net::LDAP.stub_any_instance(:bind, true) do
-      VCR.use_cassette("#{user.email}_sign_in") do
+      VCR.use_cassette("#{user.email}_sign_in_using_#{Capybara.current_driver}") do
         visit new_user_session_path
         fill_in 'Chapman Username', with: user.email
         fill_in 'Password', with: 'anything'
@@ -14,4 +14,23 @@ module IntegrationHelpers
     visit root_url
     click_link 'Sign Out'
   end
+
+  def materialize_select(option_to_select, options)
+    id = options[:from]
+    parent = find(:xpath, "//*[@id=\"#{id}\"]/..")
+    parent.click
+    within parent do
+      first('li', text: option_to_select).click
+    end
+  end
+
+  def wait_for_ajax
+      Timeout.timeout(Capybara.default_max_wait_time) do
+        loop until finished_all_ajax_requests?
+      end
+    end
+
+    def finished_all_ajax_requests?
+      page.evaluate_script('jQuery.active').zero?
+    end
 end
