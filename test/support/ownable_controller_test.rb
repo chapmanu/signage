629d89fc25804b5e4200596e330fd33f66a7a8b9
Matt Congel <congel@chapman.edu>
2016-02-04 14:16:17 -0800

    tests to verify emails being sent

--- test/support/ownable_controller_test.rb
+++ test/support/ownable_controller_test.rb
@@ -1,14 +1,22 @@
 module OwnableControllerTest
   def test_should_add_an_owner
     length = @owned_object.users.length
-    post :add_owner, id: @owned_object, format: :js, user_id: users(:one).id
+
+    assert_difference 'ActionMailer::Base.deliveries.size' do
+      post :add_owner, id: @owned_object, format: :js, user_id: users(:one).id
+    end
+
     assert_equal length + 1, @owned_object.users.count
   end
 
   def test_should_remove_an_owner
     @owned_object.users << users(:one)
     length = @owned_object.users.length
-    delete :remove_owner, id: @owned_object, format: :js, user_id: users(:one).id
+    
+    assert_difference 'ActionMailer::Base.deliveries.size' do
+      delete :remove_owner, id: @owned_object, format: :js, user_id: users(:one).id
+    end
+
     assert_equal length - 1, @owned_object.users.count
   end
 end
