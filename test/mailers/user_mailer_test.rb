d4171d7b0a1b3111547e934908bd9d436ae3abc9
Matt Congel <congel@chapman.edu>
2016-02-09 16:05:03 -0800

    fixed user mailer test

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -5,7 +5,7 @@ class UserMailerTest < ActionMailer::TestCase
     mail = UserMailer.sign_slide_request('anything for now')
     assert_equal "Sign slide request", mail.subject
     assert_equal ["to@example.org"], mail.to
-    assert_equal ["from@example.com"], mail.from
+    assert_equal ["no-reply@mail.chapman.edu"], mail.from
     assert_match "Hi", mail.body.encoded
   end
 
