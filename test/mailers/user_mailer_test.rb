6a68e1457e81dfd6cd9af8cd37a55099937295bf
James Kerr <jkerr838@gmail.com>
2016-03-17 10:44:23 -0700

    Created the email for the play slide request

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -2,11 +2,11 @@ require 'test_helper'
 
 class UserMailerTest < ActionMailer::TestCase
   test "sign_slide_request" do
-    mail = UserMailer.sign_slide_request('anything for now')
-    assert_equal "Sign slide request", mail.subject
-    assert_equal ["to@example.org"], mail.to
+    signs(:one).owners << users(:ross)
+    mail = UserMailer.sign_slide_request(sign_slide: sign_slides(:one), requestor: users(:james))
+    assert_equal "Play Slide Request: MyString", mail.subject
+    assert_equal ["loehner@chapman.edu"], mail.to
     assert_equal ["no-reply@mail.chapman.edu"], mail.from
-    assert_match "Hi", mail.body.encoded
+    assert_match "requesting that their slide", mail.body.encoded
   end
-
 end
