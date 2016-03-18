672214a4ab503d87a793782185141161ef8bec96
James Kerr <jkerr838@gmail.com>
2016-03-18 15:48:25 -0700

    Added airbrussh to the gemfile

--- config/deploy.rb
+++ config/deploy.rb
@@ -16,9 +16,6 @@ set :deploy_to, '/var/www/signage'
 # Default value for :scm is :git
 # set :scm, :git
 
-# Default value for :format is :pretty
-# set :format, :pretty
-
 # Default value for :log_level is :debug
 # set :log_level, :debug
 
