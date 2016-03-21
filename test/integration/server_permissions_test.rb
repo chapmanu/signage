5c07a94c87158eb804deb2bca39217eccdcd3a02
James Kerr <jkerr838@gmail.com>
2016-03-21 15:48:44 -0700

    Added permissionsfor servers, users, and emergencies controllers

--- test/integration/server_permissions_test.rb
+++ test/integration/server_permissions_test.rb
@@ -1,16 +1,17 @@
 require 'test_helper'
 
-class EmergencyPermissionsTest < Capybara::Rails::TestCase
+class ServerPermissionsTest < Capybara::Rails::TestCase
 
   test "can view when super_admin" do
     sign_in users(:super_admin)
-    visit emergencies_path
+    visit server_path
     assert_equal 200, page.status_code
   end
 
   test "cannot view when normal user" do
     sign_in users(:one)
-    visit emergencies_path
+    visit server_path
     assert_equal 403, page.status_code
   end
+
 end
