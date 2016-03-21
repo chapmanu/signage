f766c6e1afcd0853502cb23c4cd4f60dd7c752c5
James Kerr <jkerr838@gmail.com>
2016-03-21 10:50:10 -0700

    Clean up and emergencies refactor
    Got rid of admin controller
    Removed refills
    Removed marketing controller
    Refactored emergencies to be like DHH

---
+++ app/controllers/emergencies_controller.rb
@@ -0,0 +1,27 @@
+class EmergenciesController < ApplicationController
+  layout 'admin'
+
+  def index
+    @signs = Sign.all.order(:name)
+  end
+
+  def create
+    Sign.where(id: params[:sign_ids]).update_all(emergency_params)
+    redirect_to emergencies_path, alert: "Emergency message was successfully broadcasted"
+  end
+
+  def destroy
+    if params[:id] == 'all'
+      Sign.all.update_all(emergency: nil, emergency_detail: nil)
+      redirect_to emergencies_path, alert: "Emergency message successfully cleared"
+    else
+      Sign.find(params[:id]).update(emergency_params)
+      redirect_to emergencies_path, alert: "All emergency messages have been cleared"
+    end
+  end
+
+  private
+    def emergency_params
+      params.permit(:emergency, :emergency_detail)
+    end
+end
--- app/controllers/marketing_controller.rb
+++
@@ -1,7 +0,0 @@
-class MarketingController < ApplicationController
-  skip_before_action :authenticate_user!
-  layout 'marketing'
-
-  def home
-  end
-end
