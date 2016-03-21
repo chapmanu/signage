4534d5e3eb4a96d70fd7e246a30efcd0650e0469
James Kerr <jkerr838@gmail.com>
2016-03-21 15:20:14 -0700

    Added permissions for the slides

--- test/integration/delete_activity_test.rb
+++ test/integration/delete_activity_test.rb
@@ -5,6 +5,7 @@ class DeleteActivityTest < Capybara::Rails::TestCase
   setup do
     sign_in users(:james)
     signs(:one).owners << users(:james)
+    slides(:one).owners << users(:james)
   end
 
   test "creating and deleting a slide" do
