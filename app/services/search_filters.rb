223c998384dd51f6a19a2cd82661b7c24cdb9482
Matt Congel <congel@chapman.edu>
2016-04-20 14:29:13 -0700

    made sign & slide filters default to 'mine' and not 'all'

--- app/services/search_filters.rb
+++ app/services/search_filters.rb
@@ -2,16 +2,16 @@ class SearchFilters
 
   def initialize(params, cookies, settings)
 
-  settings.each do |key, value|
+    settings.each do |key, value|
 
-    if !params[key.to_s].blank?
-        cookies.permanent.signed[key] = params[key.to_s]
-    elsif !cookies.signed[key].blank?
-        params[key.to_s] = cookies.signed[key]
-    else
-        cookies.permanent.signed[key] = value[0]
-        params[key.to_s] = cookies.signed[key]
+      if !params[key.to_s].blank?
+          cookies.permanent.signed[key] = params[key.to_s]
+      elsif !cookies.signed[key].blank?
+          params[key.to_s] = cookies.signed[key]
+      else
+          cookies.permanent.signed[key] = value[0]
+          params[key.to_s] = cookies.signed[key]
+      end
     end
-  end
   end
 end
