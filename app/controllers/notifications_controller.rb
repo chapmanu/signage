e8557e74a96708a6488a790b64202027c077cee9
Matt Congel <congel@chapman.edu>
2016-04-21 17:34:27 -0700

    created notifications page and nav option

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -6,4 +6,9 @@ class NotificationsController < ApplicationController
     @unapproved_sign_slides = SignSlide.joins(sign: :sign_users).eager_load(:sign, :slide).where('sign_users.user_id' => current_user.id).where(approved: false)
     @activites = PublicActivity::Activity.order(created_at: :desc).limit(20)
   end
+
+  def notifications
+    @unapproved_sign_slides = SignSlide.joins(sign: :sign_users).eager_load(:sign, :slide).where('sign_users.user_id' => current_user.id).where(approved: false)
+    render :index
+  end
 end
