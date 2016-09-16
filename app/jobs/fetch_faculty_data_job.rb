bfb63669701a4d9ba045c30f5fc31b99210f849c
Matt Congel <congel@chapman.edu>
2016-09-16 14:41:26 -0700

    update faculty sync job to delete faculty from table before syncing them

--- app/jobs/fetch_faculty_data_job.rb
+++ app/jobs/fetch_faculty_data_job.rb
@@ -18,6 +18,7 @@ class FetchFacultyDataJob < ActiveJob::Base
   }
 
   def perform
+    Faculty.delete_all
     xml = RestClient.get(FetchFacultyDataJob::URL)
     doc = Nokogiri::XML(xml)
     doc.xpath('//FacultyMember').each do |node|
