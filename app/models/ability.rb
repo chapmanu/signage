945c647f2558699d1aa096cc34cd5c2b09fffe58
Matt Congel <congel@chapman.edu>
2016-12-01 15:03:55 -0800

    updated CanCanCan abilities to disallow unprivileged users from viewing a hidden sign in any way

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,46 +4,28 @@ class Ability
   def initialize(user)
     user ||= User.new
 
+    alias_action :read, :play, :to => :view
+
+    # Privileged users can do these things
     if user.super_admin?
         can :manage, :all
-    end
-
-    can :manage, Sign do |sign|
-        sign.users.include?(user)
     end
 
     can :manage, Slide do |slide|
         slide.users.include?(user)
     end
+    
+    can :manage, Sign do |sign|
+        sign.users.include?(user)
+    end
 
     # All users can do these things
-    can [:read, :create, :play], Sign
+    can :create, Sign
+    can :view, Sign, :visibility => 'listed'
     can [:read, :create, :preview, :send_to_sign], Slide
-    # Define abilities for the passed in user here. For example:
-    #
-    #   user ||= User.new # guest user (not logged in)
-    #   if user.admin?
-    #     can :manage, :all
-    #   else
-    #     can :read, :all
-    #   end
-    #
-    # The first argument to `can` is the action you are giving the user
-    # permission to do.
-    # If you pass :manage it will apply to every action. Other common actions
-    # here are :read, :create, :update and :destroy.
-    #
-    # The second argument is the resource the user can perform the action on.
-    # If you pass :all it will apply to every resource. Otherwise pass a Ruby
-    # class of the resource.
-    #
-    # The third argument is an optional hash of conditions to further filter the
-    # objects.
-    # For example, here the user can only update published articles.
-    #
-    #   can :update, Article, :published => true
-    #
-    # See the wiki for details:
+
+    # See the wikis for details:
     # https://github.com/CanCanCommunity/cancancan/wiki/Defining-Abilities
+    # https://github.com/CanCanCommunity/cancancan/wiki/Ability-Precedence
   end
 end
