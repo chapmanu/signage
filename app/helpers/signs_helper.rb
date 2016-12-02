cbef5d05363b9b8f7f11d71df3a0328859cd7cc7
Matt Congel <congel@chapman.edu>
2016-12-01 16:05:58 -0800

    moved lengthy conditional logic in sign partial to helper method

--- app/helpers/signs_helper.rb
+++ app/helpers/signs_helper.rb
@@ -7,4 +7,10 @@ module SignsHelper
       "#{sign.name} <strong>(requires approval)</strong>".html_safe
     end
   end
+
+  def sign_status_orb(sign)
+    orb_title = sign.hidden? ? 'Sign is hidden' : sign.active? ? 'Sign is live' : 'Sign is inactive'
+    orb_class = sign.hidden? ? 'sign-hidden' : sign.active? ? 'sign-active' : 'sign-inactive'
+    content_tag :div, nil, title: orb_title, class: orb_class
+  end
 end
