b55044a0f1fadac40edc0b60d07cf45f33448aad
Matt Congel <congel@chapman.edu>
2016-12-02 16:53:59 -0800

    @activites -> @activities...
    come on now @jameskerr ;)

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,7 @@ class HomeController < ApplicationController
 
 	def index
 		@unapproved_sign_slides = SignSlide.joins(sign: :sign_users).eager_load(:sign, :slide).where('sign_users.user_id' => current_user.id).where(approved: false)
-		@activites = PublicActivity::Activity.order(created_at: :desc).limit(20)
+		@activities = PublicActivity::Activity.order(created_at: :desc).limit(20)
     render 'notifications/index'
 	end
 end
