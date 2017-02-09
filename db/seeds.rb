4723e9a71001fa2c2f552850fbe86a7404a50a7a
Tom Atwell <atwell@chapman.edu>
2017-02-09 11:27:22 -0800

    Seeds now creates 3 role-based users.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+# This file should contain all the record creation needed to seed the database with its default
+# values. The data can then be loaded with the rake db:seed (or created alongside the db with
+# db:setup).
 
-james = User.where(email: 'jkerr@chapman.edu').first_or_initialize
-james.first_name = 'James'
-james.last_name  = 'Kerr'
-james.role = :super_admin
-james.save
+# Users
+# Create 3 users: super_admin, sign_owner, non_sign_owner
+User.destroy_all
+
+# Skip authentication: http://stackoverflow.com/a/21169179/6763239
+super_admin = User.new(email: 'super_admin@chapman.edu',
+                       first_name: 'Super',
+                       last_name: 'Admin')
+super_admin.role = :super_admin
+super_admin.save!
+
+User.create!(email: 'sign_owner@chapman.edu', first_name: 'Sign', last_name: 'Owner')
+User.create!(email: 'non_sign_owner@chapman.edu', first_name: 'Non', last_name: 'Owner')
