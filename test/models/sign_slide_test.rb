10069189c0104f3260f0398ce62b26ec27c63898
Tom Atwell <atwell@chapman.edu>
2017-02-10 14:49:52 -0800

    Orders notifications by stop_on field.

--- test/models/sign_slide_test.rb
+++ test/models/sign_slide_test.rb
@@ -75,4 +75,37 @@ class SignSlideTest < ActiveSupport::TestCase
     assert_not_includes sign_slides, slide_awaiting_approval.sign_slides.first
     assert_not_includes sign_slides, slide_not_awaiting_approval.sign_slides.first
   end
+
+  test "expects sign owners notifications to be ordered by stop_on field" do
+    # Arrange
+    sign = signs(:build_team_area)
+    slide_expires_tomorrow = slides(:awaiting_approval)
+    slide_expires_in_week = slide_expires_tomorrow.dup
+    slide_without_stop_date = slides(:without_stop)
+    expired_slide = slides(:expired)
+    sign_owner = users(:sign_owner)
+
+    slide_expires_tomorrow.update(stop_on: Time.zone.now + 1.day)
+    slide_expires_in_week.update(stop_on: Time.zone.now + 1.week)
+
+    sign.owners << sign_owner
+    sign.slides << slide_expires_tomorrow
+    sign.slides << slide_expires_in_week
+    sign.slides << slide_without_stop_date
+    sign.slides << expired_slide
+    sign.save!
+
+    # Assume
+    assert_equal 4, sign.slides.count
+    assert_includes sign.owners, sign_owner
+    expected_sign_slides_order = [slide_expires_tomorrow.sign_slides.first,
+                                  slide_expires_in_week.sign_slides.first,
+                                  slide_without_stop_date.sign_slides.first]
+
+    # Act
+    sign_slides = SignSlide.awaiting_approval_by_sign_owner(sign_owner).to_a
+
+    # Assert
+    assert_equal expected_sign_slides_order, sign_slides
+  end
 end
