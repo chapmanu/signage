66a3690e30de18600ad9e7fd426ea4563f47a680
mirabelrice <rice122@mail.chapman.edu>
2017-02-28 10:18:34 -0800

    implemented vertical orientation for social feed template

--- test/integration/slide_integration_test.rb
+++ test/integration/slide_integration_test.rb
@@ -3,6 +3,9 @@ require 'test_helper'
 class SlideIntegrationTest < Capybara::Rails::TestCase
   let(:user) { users(:james) }
   let(:slide) { slides(:one)}
+  let(:vert_social_slide) { slides(:social_feed_vertical) }
+  let(:horiz_social_slide) { slides(:social_feed_horizontal) }
+  let(:standard_slide) { slides(:one) }
   let(:scheduled_item) { scheduled_items(:one) }
 
   setup do
@@ -14,7 +17,7 @@ class SlideIntegrationTest < Capybara::Rails::TestCase
     scheduled_item.update({admission: 'Free', audience: 'Students'})
     slide.update(template: 'schedule')
     slide.scheduled_items << scheduled_item
-    
+
     visit preview_slide_path(slide)
     assert page.has_css?('img#ui-event-admission-icon'), "Admission icon is not present"
     assert page.has_css?('img#ui-event-audience-icon'), "Audience icon is not present"
@@ -28,5 +31,33 @@ class SlideIntegrationTest < Capybara::Rails::TestCase
     visit preview_slide_path(slide)
     assert page.has_no_css?('img#ui-event-admission-icon'), "Admission icon is present"
     assert page.has_no_css?('img#ui-event-audience-icon'), "Audience icon is present"
+  end
+
+  describe "Selecting slide orientation" do
+    before { sign_in(users(:super_admin)) }
+
+    it "Social Feed slide can select vertical orientation", js: true do
+      visit edit_slide_path(vert_social_slide)
+      assert page.has_content?("Select slide orientation")
+    end
+
+    it "Standard slide can not select vertical orientation", js: true do
+      visit edit_slide_path(standard_slide)
+      assert page.has_no_content?("Select slide orientation")
+    end
+  end
+
+  describe "Slide orientation when previewing social feed slide" do
+    before{ sign_in(users(:super_admin)) }
+
+    it "uses vertical style", js: true do
+      visit preview_slide_path(vert_social_slide)
+      assert page.has_css?("div.social-feed-container.vertical")
+    end
+
+    it "uses horizontal style", js: true do
+      visit preview_slide_path(horiz_social_slide)
+      assert page.has_css?("div.social-feed-container.horizontal")
+    end
   end
 end
