231232a4a20aff5355b905eed686bf67e3b7b7ba
ziegl107 <ziegl107@mail.chapman.edu>
2017-04-04 08:02:05 -0700

    Revert "Removes VCR from test, still failing"
    This reverts commit 237ba0b8ac3529607f1a84a95f7579887ad84c1b.

--- test/integration/emergency_permissions_test.rb
+++ test/integration/emergency_permissions_test.rb
@@ -3,36 +3,12 @@ require 'test_helper'
 class EmergencyPermissionsTest < Capybara::Rails::TestCase
 
   test "can view when super_admin" do
-    # Arrange
-    # sign_in users(:super_admin)
-    
-    # ldap_data = {"username": "super_admin", "email": users(:super_admin).email}
-    # p ldap_data.to_s
-    
-    ldap_data = '{"username": "super_admin", "email": "super_admin@chapman.edu"}'
-    stub_request(:any, "https://notify.bugsnag.com/")
-    stub_request(:any, "https://chapmanedu%2Fadbind400:yedvodX4lZBCOeqmMcBm@webfarm.chapman.edu/RoleIdentity/api/Role/super_admin").to_return(body: ldap_data)
-
-    Net::LDAP.stub_any_instance(:bind, true) do
-      visit new_user_session_path
-      fill_in 'Chapman Username', with: users(:super_admin).email
-      fill_in 'Password', with: "anything"
-      click_button 'Log in'
-    end
-    
-    p page.body
-
-    # Act
+    sign_in users(:super_admin)
     visit emergencies_path
-
-    # Assert
     assert_equal 200, page.status_code
-    assert !(page.body.include? "You need to sign in"), "expects user to be signed in"
-
   end
 
   test "cannot view when normal user" do
-    skip
     sign_in users(:james)
     visit emergencies_path
     assert_equal 403, page.status_code
