26ed5b3a873d7a6913cad47471d133d3b0c4f85a
ziegl107 <ziegl107@mail.chapman.edu>
2017-05-01 11:46:13 -0700

    changed to using scope for unexpired slides

--- app/models/sign_slide.rb
+++ app/models/sign_slide.rb
@@ -10,8 +10,14 @@ class SignSlide < ActiveRecord::Base
     .where('slides.stop_on IS NULL OR slides.stop_on > ?', Time.zone.now)
     .order('slides.stop_on ASC')
   end
+  scope :unexpired, -> do
+    eager_load(:slide)
+    .where('slides.stop_on IS NULL OR slides.stop_on > ?', Time.zone.now)
+    .order(:order)
+  end
 
   def self.awaiting_approval_by_sign_owner(sign_owner)
     awaiting_approval.joins(sign: :sign_users).where('sign_users.user_id' => sign_owner.id)
   end
+
 end
