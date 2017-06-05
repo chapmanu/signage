a5f82ddf5807e7903d5f61b582b95b53ff80aa8d
mirabelrice <rice122@mail.chapman.edu>
2017-06-05 14:32:29 -0700

    added order attribute to scheduled_items

--- app/models/scheduled_item.rb
+++ app/models/scheduled_item.rb
@@ -1,9 +1,11 @@
 class ScheduledItem < ActiveRecord::Base
+  include Schedulable
+
   belongs_to :slide, touch: true
 
   mount_uploader :image, ImageUploader
 
-  include Schedulable
+  default_scope{ order(:order) }
 
   def image_url
     Rails.application.config.asset_url + image
