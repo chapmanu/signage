28dad36a61599b8e99dfc9e570c18b1a4c495095
kziegler <ziegl107@mail.chapman.edu>
2017-06-06 15:26:55 -0700

    fixed spacing and added comments

--- test/integration/preview_test.rb
+++ test/integration/preview_test.rb
@@ -1,34 +1,33 @@
 require 'test_helper'
 
 class PreviewTest < Capybara::Rails::TestCase
-
-
-	scenario "notifications have slide preview for sign owners" do 
-		# users ross and james are the only users that actually successfully log in
-		# Arrange
-		@sign = signs(:default)
-		@sign.slides << slides(:awaiting_approval)
-		@sign.owners << users(:ross)
+  scenario "notifications have slide preview for sign owners" do 
+    # users ross and james are the only users that actually successfully log in
+	# Arrange
+	@sign = signs(:default)
+	@sign.slides << slides(:awaiting_approval)
+	@sign.owners << users(:ross)
 		
-		# Act
-		sign_in users(:ross)
+	# Act
+	sign_in users(:ross)
 
-		#Assume
-		assert page.has_content?("Sign Out"), "User did not successfully log in"
+	#Assume
+	assert page.has_content?("Sign Out"), "User did not successfully log in"
 
-		# Assert
-		assert page.has_css?('div.slide-preview-wrapper iframe'), "Preview is not present for sign owner"
-    end
+	# Assert
+	# Default page after logging in contains notifications/preview that is being tested, don't need to navigate to any other page
+	assert page.has_css?('div.slide-preview-wrapper iframe'), "Preview is not present for sign owner"
+  end
 
-	scenario "notifications do not have slide preview for non sign owners" do 
-		# Act
-		sign_in users(:james)
+  scenario "notifications do not have slide preview for non sign owners" do 
+	# Act
+	sign_in users(:james)
 
-		#Assume
-		assert page.has_content?("Sign Out"), "User did not successfully log in"
+	#Assume
+	assert page.has_content?("Sign Out"), "User did not successfully log in"
 
-		# Assert
-		assert page.has_no_css?('div.slide-preview-wrapper iframe'), "Preview is present for non sign owner"
-	end
+	# Assert
+	assert page.has_no_css?('div.slide-preview-wrapper iframe'), "Preview is present for non sign owner"
+  end
 
 end
