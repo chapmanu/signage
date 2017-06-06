0ac595493907022256b27ec9ec2e208646835d4e
kziegler <ziegl107@mail.chapman.edu>
2017-06-06 15:15:22 -0700

    rewrote test and commented

--- test/integration/preview_test.rb
+++ test/integration/preview_test.rb
@@ -2,31 +2,32 @@ require 'test_helper'
 
 class PreviewTest < Capybara::Rails::TestCase
 
-	setup do
-		signs(:default).update(name: 'Ross Sign')
-    	signs(:default).slides.clear
-		users(:ross).signs << signs(:default)
-	end
 
 	scenario "notifications have slide preview for sign owners" do 
-		sign_in users(:james)
-    	
-    	visit new_slide_path
-    	fill_in 'slide_menu_name', with: 'Non Sign Owner Slide'
-    	select 'Standard', from: 'slide_template'
-    	click_button 'Next'
-    	select 'Ross Sign (requires approval)'
-    	click_button 'Update Slide'
-    	sign_out
+		# users ross and james are the only users that actually successfully log in
+		# Arrange
+		@sign = signs(:default)
+		@sign.slides << slides(:awaiting_approval)
+		@sign.owners << users(:ross)
 		
+		# Act
 		sign_in users(:ross)
 
+		#Assume
+		assert page.has_content?("Sign Out"), "User did not successfully log in"
+
+		# Assert
 		assert page.has_css?('div.slide-preview-wrapper iframe'), "Preview is not present for sign owner"
-	end
+    end
 
 	scenario "notifications do not have slide preview for non sign owners" do 
+		# Act
 		sign_in users(:james)
 
+		#Assume
+		assert page.has_content?("Sign Out"), "User did not successfully log in"
+
+		# Assert
 		assert page.has_no_css?('div.slide-preview-wrapper iframe'), "Preview is present for non sign owner"
 	end
 
