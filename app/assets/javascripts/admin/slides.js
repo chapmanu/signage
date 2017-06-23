c5e655786ffcf290ca2a4188555b05bc01432fd8
Kayla Ziegler <kziegler@chapman.edu>
2017-06-23 11:09:41 -0700

    fixed add owner feature, commented code

--- app/assets/javascripts/admin/slides.js
+++ app/assets/javascripts/admin/slides.js
@@ -150,23 +150,42 @@ AdminSlides.initSlideActionMenus = function() {
 };
 
 
-
+/**
+ *  Ugly Temporary Add Owner Callback
+ *  This is a temporary fix to allow users to add owners to slides. The issue is connected to the jQuery autocomplete not working.
+ *  See GitHub issue: https://github.com/chapmanu/signage/issues/183
+ *  There is believed to be a naming conflict with jQuery autocomplete and the Materialize autocomplete function that was added in version 0.98.0
+ *  This fix may be able to be removed when Materialize fixes the naming issue on their end.
+ *  The same uglyTempAddOwnerCallback function is used in signs.js
+ */
 AdminSlides.uglyTempAddOwnerCallback = function(e) {
-  // doesn't wait
-
   $('#add_owner').on('click', function(e) {
+
+    // stop all
+    e.preventDefault();
+
     var user = $('#search_users').val();
 
-    var $this = $(this);
+    $.ajax({
+      url: '/slides/autocomplete_user_email?term=' + user,
+      type: "GET",
+    }).done(function(returnedData) {
+      // if returnedData not null
+      if($.trim(returnedData)){
+        returnedData.forEach(function(element) {
 
-    if ($this.data("executing")) return;
-    $this.data("executing", true);
-
-    $.get('/slides/autocomplete_user_email?term=' + user, function(returnedData) {
-      console.log(returnedData[0].id);
-      $('input#user_id').val(returnedData[0].id);
-      $this.removeData("executing");
+          //check for exact match email, exclude partials
+          if(element.email === user){
+            $('input#user_id').val(element.id);
+            $("#add_owner").submit();
+          }
+        });
+      }
+      else{
+        Materialize.toast("User "+ user +" could not be found", 4000)
+      }
     });
+
   });
 }
 
