0a76d2f8d737d095f17e80b42fbe29499af5d2d7
khafa100 <khafa100@mail.chapman.edu>
2017-08-15 11:51:34 -0700

    Fixes tests and adds migration to swap audience and admission column names.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170526192928) do
+ActiveRecord::Schema.define(version: 20170815175201) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -68,8 +68,8 @@ ActiveRecord::Schema.define(version: 20170526192928) do
     t.string   "image"
     t.string   "name"
     t.text     "content"
-    t.string   "admission"
     t.string   "audience"
+    t.string   "admission"
     t.string   "location"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
