caf4c775a612df796e522ec189f4db63f9c23039
Tom Atwell <atwell@chapman.edu>
2017-09-06 10:35:49 -0700

    Adds Public Safety feed to config files.

--- app/services/public_safety.rb
+++ app/services/public_safety.rb
@@ -1,15 +1,28 @@
-class PublicSafety
+#
+# Service class pattern based on Engine Yard example:
+# http://www.engineyard.com/blog/keeping-your-rails-controllers-dry-with-services
+#
+class PublicSafetyService
   # Static/Class Methods
   class << self
-    def rave_alert?
+    # This is the title the Chapman alert feed is expected to have if there is no
+    # emergency.
+    # TODO: Check with Public Safety to see if this message if machine or user
+    # generated. If user-generated, this is dangerous since it means user inputs
+    # it once the emergency is complete and if they don't input it as expected,
+    # signs would continue to display emergency alert.
+    NO_EMERGENCY_TITLE = 'There is currently no emergency.'.freeze
+
+    def emergency_alert?
+      latest_emergency_feed_message.title != NO_EMERGENCY_TITLE
     end
 
-    def latest_rave_feed_message
+    def latest_emergency_feed_message
     end
 
     private
 
-    def rave_feed
+    def emergency_feed
     end
   end
   # End Static/Class Methods
