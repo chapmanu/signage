cc0d739db1e3c93337083b39765037424b5825a9
Tom Atwell <atwell@chapman.edu>
2017-09-06 10:36:08 -0700

    Stubs out PublicSafetyService test.

---
+++ test/services/public_safety_service_test.rb
@@ -0,0 +1,12 @@
+# test/services/public_safety_service_test.rb
+
+require 'test_helper'
+
+class PublicSafetyServiceTest < ActiveSupport::TestCase
+  test 'expects emergency alert' do
+    raise 'TODO'
+  end
+
+  test 'expects no emergency alert' do
+  end
+end
