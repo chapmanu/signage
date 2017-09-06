284aa3e9f564f64f3683e502b07444d78329b83c
Tom Atwell <atwell@chapman.edu>
2017-09-06 10:10:58 -0700

    Stubs out PublicSafety service to handle Rave feed.

---
+++ app/services/public_safety.rb
@@ -0,0 +1,16 @@
+class PublicSafety
+  # Static/Class Methods
+  class << self
+    def rave_alert?
+    end
+
+    def latest_rave_feed_message
+    end
+
+    private
+
+    def rave_feed
+    end
+  end
+  # End Static/Class Methods
+end
