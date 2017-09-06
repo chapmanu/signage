1768da4ce95ff869b31dda62282745fee55cfbbd
Tom Atwell <atwell@chapman.edu>
2017-09-06 12:02:35 -0700

    PublicSafetyService tests pass.

--- test/services/public_safety_service_test.rb
+++ test/services/public_safety_service_test.rb
@@ -4,9 +4,28 @@ require 'test_helper'
 
 class PublicSafetyServiceTest < ActiveSupport::TestCase
   test 'expects emergency alert' do
-    raise 'TODO'
+    # Arrange
+    turn_vcr_off
+
+    # Act
+    mock_public_safety_emergency_alert
+
+    # Assert
+    assert PublicSafetyService.emergency_alert?
+
+    # Rearrange
+    turn_vcr_on
   end
 
   test 'expects no emergency alert' do
+    turn_vcr_off
+
+    # Act
+    mock_no_public_safety_emergency_alert
+
+    # Assert
+    assert_not PublicSafetyService.emergency_alert?
+
+    turn_vcr_on
   end
 end
