97cd3d14bcedadfd04dd03e61ba772a3a87c043b
Kayla Ziegler <kziegler@chapman.edu>
2017-10-09 15:00:56 -0700

    add config files for virtualbox

---
+++ config/deploy/virtualbox.rb
@@ -0,0 +1,26 @@
+#
+# This deploy script is for a VirtualBox VM running on your local machine.
+# See the dev Ansible script for more info.
+#
+# USAGE:
+#   vboxmanage startvm redhat7 --type headless
+#   cap vm deploy
+#
+# Roles are where the various servers live
+server "localhost", user: "deploy", roles: %w{web}, port: 2222
+
+# Rbenv is under user.
+set :rbenv_type, :user
+set :rbenv_ruby, '2.4.1'
+set :rbenv_map_bins, %w{rake gem bundle ruby rails}
+set :rbenv_roles, [:web]
+
+# The git branch for staging
+def current_git_branch
+  branch = `git symbolic-ref HEAD 2> /dev/null`.strip.gsub(/^refs\/heads\//, '')
+  puts "Deploying branch #{branch}"
+  branch
+end
+
+# Set the deploy branch to the current branch
+set :branch, current_git_branch
