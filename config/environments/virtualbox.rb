a773af537e6da9ebebaf00b68fbb776cda046332
Kayla Ziegler <kziegler@chapman.edu>
2017-10-24 13:41:05 -0700

    small config changes based on virtualbox environment

--- config/environments/virtualbox.rb
+++ config/environments/virtualbox.rb
@@ -71,3 +71,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
+
+Rails.application.routes.default_url_options[:host] = 'localhost:8080'
