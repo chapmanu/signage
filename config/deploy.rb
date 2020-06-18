f1eb7737d547ff1785eee1c51ba59b300622182c
Nicholas <nicholasnadel@gmail.com>
2020-06-18 15:34:55 -0700

    scratch that

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,6 @@
 # config valid only for current version of Capistrano
 #lock '3.4.0'
-
+:passenger_in_gemfile
 set :application, 'signage'
 set :repo_url, 'git@github.com:chapmanu/signage.git'
 
@@ -33,6 +33,8 @@ set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/upl
 
 # Default value for keep_releases is 5
 # set :keep_releases, 5
+
+set :passenger_restart_with_touch, true
 
 namespace :deploy do
 
